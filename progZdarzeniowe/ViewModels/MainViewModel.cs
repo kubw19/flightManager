c42f031a285fbec39ee1145399fe1b30dffefd5c
Kuba Włodarczyk <kubw19@gmail.com>
2019-10-30 20:47:54 +0100

    Ładne zmiany

---
+++ progZdarzeniowe/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace progZdarzeniowe.ViewModels
+{
+    class MainViewModel : Conductor<object>
+    {
+
+        public MainViewModel()
+        {
+            ActivateItem(new BookFlightViewModel());
+        }
+    }
+}
---
+++ progZdarzeniowe/Views/MainView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace progZdarzeniowe.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseApplication_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+    }
+}
