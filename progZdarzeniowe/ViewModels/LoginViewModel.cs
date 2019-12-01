d0be16f333fa7a1559007b0db213e344e3f8f42b
Jakub Włodarczyk <kubw19@gmail.com>
2019-12-01 10:28:32 +0100

    Całkiem duże zmiany

---
+++ progZdarzeniowe/ViewModels/LoginViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace progZdarzeniowe.ViewModels
+{
+    class LoginViewModel : Screen
+    {
+        public void emailClick(TextBox email)
+        {
+            email.Text = "";
+        }
+        public void emailLeave(TextBox email)
+        {
+            if (email.Text == "") email.Text = "Email";
+        }
+
+        public void passwordClick(TextBox password)
+        {
+            password.Text = "";
+        }
+        public void passwordLeave(TextBox password)
+        {
+            if (password.Text == "") password.Text = "Password";
+        }
+    }
+}
--- progZdarzeniowe/ViewModels/MainViewModel.cs
+++ progZdarzeniowe/ViewModels/MainViewModel.cs
@@ -9,16 +9,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace progZdarzeniowe.ViewModels
 {
     class MainViewModel : Conductor<object>
     {
+        private Button bookFlightButton { get; set; }
+        private Button manageFlightsButton { get; set; }
+
+        public void buttonInitialize(Button but, string id = "manageFlightsButton")
+        {
+            Console.WriteLine("asd");
+            switch (id)
+            {
+                case "bookFlight":
+                    this.bookFlightButton = but;
+                    bookFlightButton.IsEnabled = false;
+                    break;
+                case "manageFlightsButton":
+                    this.manageFlightsButton = but;
+                    manageFlightsButton.IsEnabled = false;
+                    break;
+            }
+        }
+
         public MainViewModel()
         {
             Database.OpenSession();
+            ActivateItem(new LoginViewModel());
+        }
+
+        public void manageFlights()
+        {
+            ActivateItem(new AdminViewModel());
+        }
+
+        public void bookFlight()
+        {
             ActivateItem(new UserPanelViewModel());
         }
 
+        public void login()
+        {
+            ActivateItem(new LoginViewModel());
+        }
+        
     }
 }
